79317c32a420d497f87ca085ae78aadb66e718df
atjhoendz <achunarmando@gmail.com>
2021-09-13 21:19:42 +0700

    chore: change logger level to debug

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,7 +3,7 @@ const { createLogger, format, transports } = winston;
 
 const logger = createLogger({
   format: format.combine(format.colorize({ all: true }), format.simple()),
-  transports: [new transports.Console()],
+  transports: [new transports.Console({ level: 'debug' })],
 });
 
 export default logger;
